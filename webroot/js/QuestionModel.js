2db1816736a0f6e500771f93c0be3274804cb9ee
thiiozz <vermerschth@gmail.com>
2015-03-20 11:30:30 +0100

    Update QuestionModel : answer null

--- webroot/js/QuestionModel.js
+++ webroot/js/QuestionModel.js
@@ -1,7 +1,7 @@
 function QuestionModel(id, questionToAdd){
     this.id = id;
     this.question = questionToAdd;
-    this.answer = "Réponse en cours de traitement, veuillez revenir plus tard pour consulter la réponse !";
+    this.answer = null;
     this.date = new Date().toString();
     this.processing = true;
 }
